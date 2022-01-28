c506905e761d99c90a69c93071178e59b2b2e590
Nicolas Primeau <nicolas.primeau@gmail.com>
2022-01-28 16:19:59 -0500

    k

--- src/Java11Handler.java
+++ src/Java11Handler.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.IOException;
@@ -12,6 +14,8 @@ import java.lang.RuntimeException;
 import java.net.URL;
 
 import javax.tools.JavaCompiler;
+import javax.tools.Diagnostic;
+import javax.tools.DiagnosticCollector;
 import javax.tools.StandardJavaFileManager;
 import javax.tools.StandardLocation;
 import javax.tools.ToolProvider;
@@ -53,32 +57,54 @@ class Request {
 public class Handler implements RequestHandler<Request, Response>{
   Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
+  private static String compileErrorMessage = "";
+
   @Override
   public Response handleRequest(Request event, Context context)
   {
-    System.out.println(event.body);
+    compileErrorMessage = "";
+
     Map<String, String> body = gson.fromJson(event.body, Map.class);
     try {
-        File parentDirectory = loadCode(body.get("source_code"));
-        return executeCode(parentDirectory);
-    } catch (IOException e) {
+        String sourceCode = body.get("source_code");
+        File sourceFile = new File("/tmp/Main.java");
+        FileWriter writer = new FileWriter(sourceFile);
+        writer.write(sourceCode);
+        writer.close();
+
+        File parentDirectory = loadCode(sourceFile);
+        if (compileErrorMessage.length() > 0) {
+            sourceFile.delete();
+            return new Response(200, "", compileErrorMessage);
+        }
+        Response response = executeCode(parentDirectory);
+        sourceFile.delete();
+        return response;
+    } catch (Exception e) {
         return new Response(502, "", e.toString());
     }
   }
 
-  private static File loadCode(String sourceCode) throws IOException {
-    File sourceFile = new File("/tmp/Main.java");
-    FileWriter writer = new FileWriter(sourceFile);
-
-    writer.write(sourceCode);
-    writer.close();
-
+  private static File loadCode(File sourceFile) throws IOException {
     JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+    DiagnosticCollector<JavaFileObject> diagnosticsCollector = new DiagnosticCollector<JavaFileObject>();
+
     StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+
     File parentDirectory = sourceFile.getParentFile();
     fileManager.setLocation(StandardLocation.CLASS_OUTPUT, Arrays.asList(parentDirectory));
+
     Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(Arrays.asList(sourceFile));
-    compiler.getTask(null, fileManager, null, null, null, compilationUnits).call();
+    boolean success = compiler.getTask(null, fileManager, diagnosticsCollector, null, null, compilationUnits).call();
+
+    if (!success) {
+        List<Diagnostic<? extends JavaFileObject>> diagnostics = diagnosticsCollector.getDiagnostics();
+        for (Diagnostic<? extends JavaFileObject> diagnostic : diagnostics) {
+            compileErrorMessage += diagnostic.getMessage(null);
+        }
+        return parentDirectory;
+    }
+
     fileManager.close();
     return parentDirectory;
   }
@@ -94,11 +120,12 @@ public class Handler implements RequestHandler<Request, Response>{
     System.setErr(new PrintStream(err));
 
     try {
-         URLClassLoader classLoader = URLClassLoader.newInstance(new URL[] { parentDirectory.toURI().toURL() });
-         Class<?> helloClass = classLoader.loadClass("Main");
+        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[] { parentDirectory.toURI().toURL() });
+        Class<?> main = classLoader.loadClass("Main");
+        classLoader.close();
 
-         Method helloMethod = helloClass.getDeclaredMethod("main");
-         helloMethod.invoke(helloClass.newInstance());
+         Method mainMethod = main.getDeclaredMethod("main");
+         mainMethod.invoke(main.newInstance());
 
     } catch (Exception e) {
       e.printStackTrace();
